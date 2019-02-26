1dcbe0cedae85ca565d9682e07def477f8d081a9
Matt Payne <Payne@MattPayne.org>
2019-02-25 20:19:35 -0600

    Removed debugging

--- game.js
+++ game.js
@@ -3,8 +3,6 @@ class Game {
   width; height;
 
   constructor(canvas) {
-    console.log('Game constructor canvas=');
-    console.log(canvas);
     this.context = canvas.getContext("2d")
     this.width = canvas.width
     this.height = canvas.height
@@ -14,9 +12,6 @@ class Game {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  console.log('About to new Game()');
   let canvas = document.getElementById('game');
-  console.log(canvas);
   let g = new Game(canvas);
-  console.log('Done with new Game()');
 });

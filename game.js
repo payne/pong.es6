2c1dc2444738dc19375ca5f99adb5a0c9f5a5473
Matt Payne <Payne@MattPayne.org>
2019-02-25 21:13:17 -0600

    Added keyName map

--- game.js
+++ game.js
@@ -1,5 +1,12 @@
 class Game {
   context; width; height; x; y;
+  keyName = {
+    32: 'space',
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  }
 
   constructor(canvas) {
     this.context = canvas.getContext("2d")

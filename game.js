c6a7aa266c4b84b263908e4059b49106fcf46919
Matt Payne <Payne@MattPayne.org>
2019-02-25 21:49:48 -0600

    Start of a keyboard handler.

--- game.js
+++ game.js
@@ -9,6 +9,7 @@ class Game {
   }
 
   constructor(canvas) {
+    this.setupListener(canvas);
     this.context = canvas.getContext("2d")
     this.width = canvas.width
     this.height = canvas.height
@@ -16,13 +17,31 @@ class Game {
     this.clear();
   }
 
+  keyDownHandler(e) {
+    console.log(e);
+  }
+
+  setupListener(canvas) {
+    console.log('setting up')
+    /*
+    document.addEventListener('keydown keyup', function (e) {
+      // Convert key code to key name
+      console.log(e);
+      var name = this.keyName[e.which]
+      console.log(name);
+    }, false);
+    */
+    document.addEventListener("keydown", this.keyDownHandler, false);
+    console.log('set up')
+  }
+
+
   clear() {
     this.context.fillStyle = 'red';
     this.context.fillRect(0, 0, this.width, this.height)
   }
 
   step() {
-    console.log('step');
     this.clear(); // comment this out for line effect
     this.context.fillStyle = 'blue';
     this.context.fillRect(this.x++, this.y++, 10, 10);

a31ceeb96884cbf458fa129b84803cd60da1cd62
Matt Payne <Payne@MattPayne.org>
2019-02-25 20:18:46 -0600

    canvas is displayed :-)

--- game.js
+++ game.js
@@ -1,11 +1,22 @@
 class Game {
-    constructor() {
-        console.log('Game constructor');
-    }
+  context;
+  width; height;
+
+  constructor(canvas) {
+    console.log('Game constructor canvas=');
+    console.log(canvas);
+    this.context = canvas.getContext("2d")
+    this.width = canvas.width
+    this.height = canvas.height
+    this.context.fillStyle = '#000'
+    this.context.fillRect(0, 0, this.width, this.height)
+  }
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-    console.log('About to new Game()');
-    let g = new Game();
-    console.log('Done with new Game()');
+document.addEventListener("DOMContentLoaded", function () {
+  console.log('About to new Game()');
+  let canvas = document.getElementById('game');
+  console.log(canvas);
+  let g = new Game(canvas);
+  console.log('Done with new Game()');
 });
